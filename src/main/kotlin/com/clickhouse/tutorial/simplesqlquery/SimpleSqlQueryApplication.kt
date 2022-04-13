5566d9052ff1bbd07bc4fbc3ceb3d9a653642075
chenguangjian.jk <chenguangjian.jk@bytedance.com>
2022-04-14 01:23:23 +0800

    feat: init

---
+++ src/main/kotlin/com/clickhouse/tutorial/simplesqlquery/SimpleSqlQueryApplication.kt
@@ -0,0 +1,11 @@
+package com.clickhouse.tutorial.simplesqlquery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleSqlQueryApplication
+
+fun main(args: Array<String>) {
+	runApplication<SimpleSqlQueryApplication>(*args)
+}
